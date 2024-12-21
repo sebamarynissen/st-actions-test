ba22d2f6c8cf7db2dcf784ca6a7824f18af5f4d3
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-21 23:48:13 +0100

    Update fetch.js

--- actions/fetch/fetch.js
+++ actions/fetch/fetch.js
@@ -4,29 +4,43 @@ import path from 'node:path';
 import core from '@actions/core';
 
 const cwd = process.env.GITHUB_WORKSPACE ?? process.cwd();
-let file = path.join(cwd, 'src/yaml/smf-16/everseasonal.yaml');
 
-await fs.promises.mkdir(path.dirname(file), { recursive: true });
-await fs.promises.writeFile(file, `
-group: smf-16
-name: everseasonal
-version: "1.0.2"
-subfolder: 150-mods
-info:
-  summary: Everseasonal
+async function make(file, contents) {
+  let fullPath = path.join(cwd, `src/yaml/${file}`);
+  await fs.promises.mkdir(path.dirname(fullPath), { recursive: true });
+  await fs.promises.writeFile(fullPath, contents);
+}
 
-assets:
-  - assetId: smf-16-everseasonal
+await make('smf-16/everseasonal.yaml', `
+  group: smf-16
+  name: everseasonal
+  version: "1.0.2"
+  # subfolder: 150-mods
+  info:
+    summary: Everseasonal
 
----
-assetId: smf-16-everseasonal
-version: "1.0.2"
-lastModified: "2024-12-21T:21:40:00Z"
-url: https://community.simtropolis.com/files/file/123-file/?do=download&r=456
+  assets:
+    - assetId: smf-16-everseasonal
+
+  ---
+  assetId: smf-16-everseasonal
+  version: "1.0.2"
+  lastModified: "2024-12-21T:21:40:00Z"
+  url: https://community.simtropolis.com/files/file/123-file/?do=download&r=456
+`);
+
+await make('jasoncw/collection.yaml', `
+  group: jasoncw
+  name: collection
+  version: "1.0.0"
+  subfolder: 200-residential
+  variants:
+    - variant: { jasoncw:collection:mode: on }
+    - variant: { jasoncw:collection:mode: off }
 `);
 
 let result = {
-  timestamp: '2024-12-21T20:26:27',
+  timestamp: new Date().toISOString(),
   packages: [
     {
       id: 'smf-16:everseasonal',
