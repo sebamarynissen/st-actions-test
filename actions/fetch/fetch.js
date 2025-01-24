1d0eb1ac1ef6c81bda603a6020956f2531ef1dab
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-24 15:51:43 +0100

    Test sending failed package message

--- actions/fetch/fetch.js
+++ actions/fetch/fetch.js
@@ -43,7 +43,7 @@ subfolder: 200-residential
 info:
   summary: Jasoncw Collection
 dependencies:
-  - memo:submenus-dll
+  - memo:submenu-dll
 variants:
   - variant: { jasoncw:collection:mode: "on" }
   - variant: { jasoncw:collection:mode: "off" }
